98647cf8bff08efab92192df1024ebd6d2d13015
Yasen Kurtev <yasenuni@abv.bg>
2022-04-27 15:20:12 +0300

    Added completed tasks and their descriptions from the current lesson.

---
+++ Regular Expressions - Lab/matchPhoneNumber.js
@@ -0,0 +1,12 @@
+function matchPhoneNumber(string) {
+
+    let pattern = /[+]359([ -])2\1\d{3}\1\d{4}\b/g;
+    let validNumbers = [];
+    let validNumber = null;
+    while ((validNumber = pattern.exec(string)) !== null) {
+        validNumbers.push(validNumber[0]);
+    }
+    console.log(validNumbers.join(', '));
+}
+
+matchPhoneNumber("+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222")
